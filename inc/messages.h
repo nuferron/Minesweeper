076555a3c1d06b2e00b946b935ee92978f241810
nuferron <138325463+nuferron@users.noreply.github.com>
2024-04-19 20:26:01 +0000

    added game mode messages + input control

---
+++ inc/messages.h
@@ -0,0 +1,17 @@
+#ifndef MESSAGES_H
+# define MESSAGES_H
+
+#define WELCOME "Welcome to Minesweeper! 💣🚩\n"
+#define MENU    "Please, enter your game mode:\n"
+#define EASY    "\t(1) Easy: 8×8 10 mines\n"
+#define MEDIUM  "\t(2) Intermediate: 16×16 40 mines\n"
+#define HARD    "\t(3) Expert: 16×30 99 mines\n"
+#define PERS    "\t(4) Personalized\n"
+#define EXIT    "\t(5) Exit\n"
+#define INVALID "Invalid number\n"
+#define BYE     "See you soon!\n"
+#define REQBOX  "Both rows and columns must have a minimum of 2 boxes and a maximum of 50.\n"
+#define REQMINE "The number of mines must be greater than 0 and less than the number of boxes.\n"
+#define FORMAT  "Please, enter your preferences in this format:\n\trow col mines\n"
+
+#endif
