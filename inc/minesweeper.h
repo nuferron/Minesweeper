610754492291c4b8030e55e5c0083854d0ea0e3f
nuferron <138325463+nuferron@users.noreply.github.com>
2024-04-19 20:26:18 +0000

    added structs

--- inc/minesweeper.h
+++ inc/minesweeper.h
@@ -3,5 +3,20 @@
 
 #include "libft/libft.h"
 #include "libft/ft_printf/ft_printf.h"
+#include "messages.h"
+
+typedef struct s_board
+{
+    int col;
+    int row;
+    int mines;
+}  t_board;
+
+typedef struct s_box
+{
+    bool    print;
+    bool    flag;
+    int     value;
+}   t_box;
 
 #endif
